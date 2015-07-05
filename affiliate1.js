203b567c21f37d12eb7423fb2b3d86f9f31b8d2d
acrosspg <acrosspg@yahoo.com>
2015-07-05 20:23:26 +0530

    Create affiliate1.js

---
+++ affiliate1.js
@@ -0,0 +1,23 @@
+var images = [], 
+index = 0;
+
+images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-2-3rd/p/itmdmfu2wk2gzwpc?pid=9788192022420&affid=acrosspgya' target='_blank'><img src='http://tiny.cc/ur7l6w' alt='DNB CET Review 3rr Edition'></a>";
+images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-10th/p/itmdnnanqtjdsatt?pid=9788189926793&affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-Ldb__mwvXeE/UoP_FSZVe9I/AAAAAAAABCQ/wXtuINWb3s8/w300-h250-no/roams-10ed.jpg' alt='ROAMS 10th Edition'></a>";
+images[2] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912321680994255' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
+images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-4th/p/itmdvzg5p4ykm8sf?pid=9788189477219&affid=acrosspgya' target='_blank'><img src='https://lh6.googleusercontent.com/-7nDOIpr24aY/U20O3tGyY-I/AAAAAAAABb8/U4-Z_Yy13b4/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25201.jpg'></a>";
+images[4] = "<a href = 'http://www.flipkart.com/motorola/motox?affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-KyC9M5mQ9zU/UytE4oife_I/AAAAAAAABXs/vAXltqaOHf0/s300/Moto-X-Flipkart.jpg' alt='Moto X'></a>";
+images[5] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-2-fourth/p/itmdvuwzsswumgnt?pid=9788189477226&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/-WRrVYp4LiT0/U20O4rNYjKI/AAAAAAAABcM/M8ix8sAEcD8/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25202.jpg'></a>";
+images[6] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-3-fourth/p/itmdvuwpfphrett4?pid=9788189477233&affid=acrosspgya' target='_blank'><img src='https://lh6.googleusercontent.com/-S7QEE1aBAIU/U20O4i6SxyI/AAAAAAAABcI/WT8MGz1Ki5A/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25203.jpg'></a>";
+images[7] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912334559088353' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
+images[8] = "<a href='http://t.groupon.co.in/r?tsToken=IN_AFF_0_204339_259147_0&url=http%3A%2F%2Fwww.groupon.co.in%2F%3FCID%3DIN_AFF_5600_225_5383_1%26nlp%26utm_medium%3Dafl%26utm_source%3DGPN%26utm_campaign%3D204339%26mediaId%3D259147'>";
+images[9] = "<iframe src='http://ws-in.amazon-adsystem.com/widgets/q?t=ac023-21&o=31&p=8&l=as1&asins=8189477218&ref=tf_til&fc1=000000&IS2=1&lt1=_blank&m=amazon&lc1=0000FF&bc1=000000&bg1=FFFFFF&f=ifr&MarketPlace=IN&ServiceVersion=20070822&WS=1&ID=8042_ProductLink&Operation=GetProductLink&'style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='0'></iframe>";
+images[10] = "<script type='text/javascript' language='javascript'> var aax_size='300x250'; var aax_pubname = 'ac023-21'; var aax_src='302'; </script><script type='text/javascript' language='javascript' src='http://c.amazon-adsystem.com/aax2/assoc.js'></script>";
+images[11] = "<iframe src='http://origin-img.shaadi.com/shaadi-rewards/get-banner.php?ptnr=zw6aj&banner_type=rectangles&banner_size=300x250&ad_type=iframe' width='300' height='250' scrolling='no' frameborder='0' marginwidth='0' marginheight='0'></iframe>";
+images[12] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138454324291031221' frameborder=0 height=250 width=300></iframe>";
+images[13] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-142615090336277268' frameborder=0 height=250 width=300></iframe>";
+images[14] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-142615095099876540' frameborder=0 height=250 width=300></iframe>";
+
+
+
+index = Math.floor(Math.random() * images.length);
+document.write(images[index]);
