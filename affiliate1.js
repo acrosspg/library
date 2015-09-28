b1c94710646f1a08117998d241f2c8c96b5db6a9
acrosspg <acrosspg@yahoo.com>
2015-09-29 03:55:19 +0530

    Update affiliate1.js

--- affiliate1.js
+++ affiliate1.js
@@ -1,11 +1,11 @@
 var images = [], 
 index = 0;
 
-images[0] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-2-3rd/p/itmdmfu2wk2gzwpc?pid=9788192022420&affid=acrosspgya' target='_blank'><img src='http://tiny.cc/ur7l6w' alt='DNB CET Review 3rr Edition'></a>";
-images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-10th/p/itmdnnanqtjdsatt?pid=9788189926793&affid=acrosspgya' target='_blank'><img src='https://lh3.googleusercontent.com/-Ldb__mwvXeE/UoP_FSZVe9I/AAAAAAAABCQ/wXtuINWb3s8/w300-h250-no/roams-10ed.jpg' alt='ROAMS 10th Edition'></a>";
+images[0] = "<a href = 'http://www.flipkart.com/across-complete-review-short-subjects-volume-1-english-8th/p/itmdx9f2mvpenzed?affid=acrosspgya' target='_blank'><img src='http://i.imgur.com/IqOmHn3.jpg' alt='Across Vol 1'></a>";
+images[1] = "<a href = 'http://www.flipkart.com/roams-review-all-medical-subjects-english/p/itme952ju25hucfa?affid=acrosspgya' target='_blank'><img src='http://i.imgur.com/Yq15v8p.jpg' alt='ROAMS 12th Edition'></a>";
 images[2] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912321680994255' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
 images[3] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-1-4th/p/itmdvzg5p4ykm8sf?pid=9788189477219&affid=acrosspgya' target='_blank'><img src='https://lh6.googleusercontent.com/-7nDOIpr24aY/U20O3tGyY-I/AAAAAAAABb8/U4-Z_Yy13b4/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25201.jpg'></a>";
-images[4] = "<a href = 'http://www.flipkart.com/motorola/motox?affid=acrosspgya' target='_blank'><img src='https://lh5.googleusercontent.com/-KyC9M5mQ9zU/UytE4oife_I/AAAAAAAABXs/vAXltqaOHf0/s300/Moto-X-Flipkart.jpg' alt='Moto X'></a>";
+images[4] = "<div data-WRID='WRID-144347858016353615' data-widgetType='staticBanner'  data-class='affiliateAdsByFlipkart' height='250' width='250'></div><script async src='//affiliate-static.flixcart.com/affiliate/widgets/FKAffiliateWidgets.js'></script>";
 images[5] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-2-fourth/p/itmdvuwzsswumgnt?pid=9788189477226&affid=acrosspgya' target='_blank'><img src='https://lh4.googleusercontent.com/-WRrVYp4LiT0/U20O4rNYjKI/AAAAAAAABcM/M8ix8sAEcD8/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25202.jpg'></a>";
 images[6] = "<a href = 'http://www.flipkart.com/dnb-cet-review-primary-post-diploma-volume-3-fourth/p/itmdvuwpfphrett4?pid=9788189477233&affid=acrosspgya' target='_blank'><img src='https://lh6.googleusercontent.com/-S7QEE1aBAIU/U20O4i6SxyI/AAAAAAAABcI/WT8MGz1Ki5A/s300/dnb%2520cet%2520review%25204th%2520ed%2520-%2520vol%25203.jpg'></a>";
 images[7] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138912334559088353' style='width:120px;height:240px;' scrolling='no' marginwidth='0' marginheight='0' frameborder='1' ></iframe>";
@@ -15,7 +15,7 @@ images[10] = "<iframe src='http://origin-img.shaadi.com/shaadi-rewards/get-banne
 images[11] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-138454324291031221' frameborder=0 height=250 width=300></iframe>";
 images[12] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-142615090336277268' frameborder=0 height=250 width=300></iframe>";
 images[13] = "<iframe src='http://www.flipkart.com/affiliate/displayWidget?affrid=WRID-142615095099876540' frameborder=0 height=250 width=300></iframe>";
-
+images[14] = "<div data-WRID='WRID-144347897833281030' data-widgetType='staticBanner' data-responsive='yes' data-class='affiliateAdsByFlipkart' height='250' width='300'></div><script async src='//affiliate-static.flixcart.com/affiliate/widgets/FKAffiliateWidgets.js'></script>";
 
 
 index = Math.floor(Math.random() * images.length);
