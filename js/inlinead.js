06c420023584705e156e12fcaad6fb72f7f35d66
acrosspg <acrosspg@yahoo.com>
2016-06-12 12:20:12 +0530

    Update inlinead.js

--- js/inlinead.js
+++ js/inlinead.js
@@ -4,14 +4,15 @@ index = 0;
 images[0] = "<a href = 'http://dl.flipkart.com/dl/essentials-medical-pharmacology-english-7th/p/itmdmpfrhwazhfag?pid=9789350259375&affid=acrosspgya'>Essentials of Medical Pharmacology-7th Edition by KD Tripathi</a>";
 images[1] = "<a href = 'http://fkrt.it/NLsB6LNN' target='_blank'>LMR LAST MINUTE REVISION SERIES FOR NBE/AIPGMEE/FMGE - Latest Edition</a>";
 images[2] = "<a href = 'http://fkrt.it/u2YhlrNN' target='_blank'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
-images[3] = "<a href = 'http://axpg.ml/18gZLJK' target='_blank'>Buy Pharmacology : Prep Manual for Undergraduates 2nd Edition</a>";
-images[4] = "<a href = 'http://axpg.ml/1zc62vy' target='_blank'>Buy Concise Pathology for Exam Preparation by Geetika Khanna Bhattacharya 2nd Edition</a>";
-images[5] = "<a href = 'http://axpg.ml/18h36IN' target='_blank'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
-images[6] = "<a href = 'http://axpg.ml/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
-images[7] = "<a href = 'http://axpg.ml/1wj9v0V' target='_blank'>SMART STUDY SERIES - Obstetrics & Gynecology 3rd Edition</a>";
-images[8] = "<a href = 'http://axpg.ml/1BLzNds' target='_blank'>AIPGMEE : Self Assessment and Review by MUDIT KHANNA (Volume - 1) 2012 - 2007 12th Edition</a>";
+images[3] = "<a href = 'http://bit.ly/18gZLJK' target='_blank'>Buy Pharmacology : Prep Manual for Undergraduates 2nd Edition</a>";
+images[4] = "<a href = 'http://dl.flipkart.com/dl/concise-pathology-exam-preparation-2ed-english-2nd/p/itmdfgy7fcwxwzqf?srno=b_4&pid=9788131228074&affid=acrosspgya&al=Fetwtp4ZlEs30j1XgNJcCcldugMWZuE7mxWx381qOwSG%2FstJVX%2FIjW2QkwoQhwUwhMYE81rP%2FqE%3D&affExtParam1=inlineads' target='_blank'>Buy Concise Pathology for Exam Preparation by Geetika Khanna Bhattacharya 2nd Edition</a>";
+images[5] = "<a href = 'http://fkrt.it/pfSEauuuuN' target='_blank'>Buy Platinum Notes - Preclinical Sciences Volume 1 (2014 - 15) 4th Edition</a>";
+images[6] = "<a href = 'http://bit.ly/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
+images[7] = "<a href = 'http://dl.flipkart.com/dl/obstetrics-gynecology-english-3rd/p/itmdxhvwzchnu6cu?srno=b_3&pid=9788131237670&affid=acrosspgya&al=Fetwtp4ZlEs30j1XgNJcCcldugMWZuE7mxWx381qOwSG%2FstJVX%2FIjWlX54PBs%2Fc5a8fGwbUV65Y%3D&affExtParam1=inlineads' target='_blank'>SMART STUDY SERIES - Obstetrics & Gynecology 3rd Edition</a>";
+images[8] = "<a href = 'http://bit.ly/1BLzNds' target='_blank'>AIPGMEE : Self Assessment and Review by MUDIT KHANNA (Volume - 1) 2012 - 2007 12th Edition</a>";
 images[9] = "<a href = 'http://bit.ly/rapidreviewpatho' target='_blank'>Rapid Review Pathology (English) 4th Edition</a>";
-images[10] = "<a href = 'http://axpg.ml/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
+images[10] = "<a href = 'http://bit.ly/1GnVteE' target='_blank'>Buy Platinum Notes - Medical Sciences Volume 2 (2014 - 15) 4th Edition</a>";
+images[11] = "<a href = 'http://dl.flipkart.com/dl/smart-study-series-ent-2-e/p/itmdfzxp9tpzsnwu?srno=b_5&pid=9788131233184&affid=acrosspgya&al=Fetwtp4ZlEuM%2BvoqrNUPFsldugMWZuE7mxWx381qOwSG%2FstJVX%2FIjRl4eKj6ffA%2BWKS5gbbsZeU%3D&affExtParam1=inlineads' target='_blank'>Smart Study Series - ENT - 2nd Edition</a>";
 
 
 
