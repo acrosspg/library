4e79bae04e718b6d344039c93370e31092050ba4
acrosspg <acrosspg@yahoo.com>
2016-06-18 12:07:31 +0530

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,17 @@
-jQuery(document).ready(function () {
+//jQuery(document).ready(function () {
+//
+//	$('#membermenu-toggle').click(function () {
+//	$('#membernav').toggleClass('open');
+//    e.preventDefault();
+//    });
+//    
+//});
 
-	$('#membermenu-toggle').click(function () {
-	$('#membernav').toggleClass('open');
-      e.preventDefault();
+jQuery(document).ready(function() {
+    jQuery('#membermenu-toggle').click(function(e) {
+        jQuery(this).toggleClass('open');
+        jQuery('#membernav').toggleClass('open');
+ 
+        e.preventDefault();
     });
-    
 });
